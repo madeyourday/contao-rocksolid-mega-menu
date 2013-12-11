6a529f2b5416109e647ff567d0c97a94869c50f2
Martin Auswöger <martin@auswoeger.com>
2013-12-11 12:13:43 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# RockSolid Mega Menu Contao Extension
+
+Install to */system/modules/rocksolid-mega-menu/*
+
+More information: 
+
+* English: http://rocksolidthemes.com/en/contao/plugins/mega-menu
+* German: http://rocksolidthemes.com/de/contao/plugins/mega-menu
---
+++ config/autoload.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * Copyright MADE/YOUR/DAY OG <mail@madeyourday.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * RockSolid Mega Menu autload configuration
+ *
+ * @author Martin Auswöger <martin@madeyourday.net>
+ */
+
+ClassLoader::addClasses(array(
+));
+
+TemplateLoader::addFiles(array(
+));
