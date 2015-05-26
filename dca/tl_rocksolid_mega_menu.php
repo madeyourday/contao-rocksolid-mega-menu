c75670623f46ab4cc24b686b88d6c602ae7e1b28
Martin Auswöger <martin@auswoeger.com>
2015-05-26 09:46:28 +0200

    Added compatibility for Contao 4

--- dca/tl_rocksolid_mega_menu.php
+++ dca/tl_rocksolid_mega_menu.php
@@ -54,7 +54,7 @@ $GLOBALS['TL_DCA']['tl_rocksolid_mega_menu'] = array(
 				'label' => &$GLOBALS['TL_LANG']['tl_rocksolid_mega_menu']['editLicense'],
 				'href' => 'table=tl_rocksolid_mega_menu_license',
 				'class' => 'header_icon',
-				'icon' => 'system/themes/default/images/settings.gif',
+				'icon' => 'system/themes/' . \Controller::getTheme() . '/images/settings.gif',
 			),
 			'all' => array(
 				'label' => &$GLOBALS['TL_LANG']['MSC']['all'],
